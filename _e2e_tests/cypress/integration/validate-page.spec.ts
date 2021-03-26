f200cfd5eaf2970fe2c7e88e0517b901b1b8de3d
Nadun De Silva <nadunrds@gmail.com>
2021-03-26 22:05:37 +0530

    Add typescript support

--- _e2e_tests/cypress/integration/validate-page.spec.ts
+++ _e2e_tests/cypress/integration/validate-page.spec.ts
@@ -1,33 +1,35 @@
 describe("Page Validation Test", () => {
     beforeEach(() => {
-        cy.visit("/")
-    })
+        cy.visit("/");
+    });
 
     it("Validates whether page loads", () => {
         cy.get("[data-cy-section=welcome-banner]")
             .should('be.visible')
             .and("contain", "Nadun")
-            .and("contain", "De Silva")
-    })
+            .and("contain", "De Silva");
+    });
 
     it("Validates all sections", () => {
         cy.fixture("sections.json").then((sections) => {
             sections.forEach((section) => {
                 cy.get("[data-cy-section=" + section.name + "]")
-                    .scrollIntoView()
-            })
-        })
-    })
+                    .scrollIntoView({
+                        duration: 1000
+                    });
+            });
+        });
+    });
 
     it("Validate nav item click", () => {
         cy.fixture("sections.json").then((sections) => {
             sections.filter((section) => section.hasNavBarItem)
                 .forEach((section) => {
                     cy.get("[data-cy-nav-item=" + section.name + "]")
-                        .click()
+                        .click();
                     cy.get("[data-cy-section=" + section.name + "]")
-                        .should('be.visible')
-                })
-        })
-    })
-})
+                        .should('be.visible');
+                });
+        });
+    });
+});
