969de0ab2d7c15636a90344be1de15fa180db60e
Nadun De Silva <nadunrds@gmail.com>
2021-03-26 21:30:20 +0530

    Add tests for validating website home sections

--- _e2e_tests/cypress/integration/validate-page.spec.js
+++ _e2e_tests/cypress/integration/validate-page.spec.js
@@ -1,5 +1,33 @@
 describe("Page Validation Test", () => {
-  it("Validates whether page loads", () => {
-    cy.visit("/")
-  })
+    beforeEach(() => {
+        cy.visit("/")
+    })
+
+    it("Validates whether page loads", () => {
+        cy.get("[data-cy-section=welcome-banner]")
+            .should('be.visible')
+            .and("contain", "Nadun")
+            .and("contain", "De Silva")
+    })
+
+    it("Validates all sections", () => {
+        cy.fixture("sections.json").then((sections) => {
+            sections.forEach((section) => {
+                cy.get("[data-cy-section=" + section.name + "]")
+                    .scrollIntoView()
+            })
+        })
+    })
+
+    it("Validate nav item click", () => {
+        cy.fixture("sections.json").then((sections) => {
+            sections.filter((section) => section.hasNavBarItem)
+                .forEach((section) => {
+                    cy.get("[data-cy-nav-item=" + section.name + "]")
+                        .click()
+                    cy.get("[data-cy-section=" + section.name + "]")
+                        .should('be.visible')
+                })
+        })
+    })
 })
