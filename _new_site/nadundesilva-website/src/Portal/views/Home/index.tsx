9f3916ef890b0c993380de40235547142c9b689f
Nadun De Silva <nadunrds@gmail.com>
2021-08-03 00:45:24 +0530

    Update tab spacing linter rules

--- _new_site/nadundesilva-website/src/Portal/views/Home/index.tsx
+++ _new_site/nadundesilva-website/src/Portal/views/Home/index.tsx
@@ -5,79 +5,79 @@ import Education from "./Education";
 import { Button, createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => {
-	const sidesPadding = 16;
-	return createStyles({
-		root: {
-			marginLeft: theme.spacing(sidesPadding),
-			marginRight: theme.spacing(sidesPadding)
-		},
-		grow: {
-			flexGrow: 1
-		},
-		section: {
-			padding: theme.spacing(1),
-			margin: theme.spacing(5)
-		},
-		sectionContent: {
-			padding: theme.spacing(3)
-		},
-		profilePhoto: {
-			width: "100%",
-			height: "auto"
-		}
-	});
+    const sidesPadding = 16;
+    return createStyles({
+        root: {
+            marginLeft: theme.spacing(sidesPadding),
+            marginRight: theme.spacing(sidesPadding)
+        },
+        grow: {
+            flexGrow: 1
+        },
+        section: {
+            padding: theme.spacing(1),
+            margin: theme.spacing(5)
+        },
+        sectionContent: {
+            padding: theme.spacing(3)
+        },
+        profilePhoto: {
+            width: "100%",
+            height: "auto"
+        }
+    });
 });
 
 interface Section {
-	name: string,
-	ref: React.RefObject<HTMLButtonElement>,
-	Component: () => React.ReactElement,
+    name: string,
+    ref: React.RefObject<HTMLButtonElement>,
+    Component: () => React.ReactElement,
 };
 
 const Home = (): React.ReactElement => {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const pageSections: Section[] = [
-		{
-			name: "Education",
-			ref: useRef<HTMLButtonElement>(null),
-			Component: Education
-		}
-	];
+    const pageSections: Section[] = [
+        {
+            name: "Education",
+            ref: useRef<HTMLButtonElement>(null),
+            Component: Education
+        }
+    ];
 
-	const generateGoToSectionHandler = (sectionRef: React.RefObject<HTMLButtonElement>) => () => {
-		sectionRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
-	};
-	return (
-		<Layout navItems={
-			<React.Fragment>
-				<div className={classes.grow} />
-				{
-					pageSections.map((section: Section) => (
-						<Button key={section.name} variant={"contained"} color="primary" disableElevation
-							ref={section.ref} onClick={generateGoToSectionHandler(section.ref)}>
-							{section.name}
-						</Button>
-					))
-				}
-			</React.Fragment>
-		}>
-			<div className={classes.root}>
-				<Typography variant="h6" align="center">About Me</Typography>
-				<AboutMe/>
-				<React.Fragment>
-					{
-						pageSections.map((section: Section) => (
-							<div className={classes.section} key={section.name}>
-								<Typography variant="h6" align="center">{section.name}</Typography>
-								<section.Component />
-							</div>
-						))
-					}
-				</React.Fragment>
-			</div>
-		</Layout>
-	);
+    const generateGoToSectionHandler = (sectionRef: React.RefObject<HTMLButtonElement>) => () => {
+        sectionRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
+    };
+    return (
+        <Layout navItems={
+            <React.Fragment>
+                <div className={classes.grow} />
+                {
+                    pageSections.map((section: Section) => (
+                        <Button key={section.name} variant={"contained"} color="primary" disableElevation
+                            ref={section.ref} onClick={generateGoToSectionHandler(section.ref)}>
+                            {section.name}
+                        </Button>
+                    ))
+                }
+            </React.Fragment>
+        }>
+            <div className={classes.root}>
+                <Typography variant="h6" align="center">About Me</Typography>
+                <AboutMe/>
+                <React.Fragment>
+                    {
+                        pageSections.map((section: Section) => (
+                            <div className={classes.section} key={section.name}>
+                                <Typography variant="h6" align="center">{section.name}</Typography>
+                                <section.Component />
+                            </div>
+                        ))
+                    }
+                </React.Fragment>
+            </div>
+        </Layout>
+    );
 };
 
 export default Home;
--- _new_site/nadundesilva-website/src/components/Layout.tsx
+++ _new_site/nadundesilva-website/src/components/Layout.tsx
@@ -3,65 +3,65 @@ import { KeyboardArrowUp as KeyboardArrowUpIcon } from "@material-ui/icons";
 import React, { useRef } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		fab: {
-			position: "fixed",
-			bottom: theme.spacing(2),
-			right: theme.spacing(2)
-		}
-	})
+    createStyles({
+        fab: {
+            position: "fixed",
+            bottom: theme.spacing(2),
+            right: theme.spacing(2)
+        }
+    })
 );
 
 interface LayoutProps {
-	children: React.ReactElement | React.ReactElement[],
-	navItems: React.ReactElement | React.ReactElement[],
-	window?: () => Window,
+    children: React.ReactElement | React.ReactElement[],
+    navItems: React.ReactElement | React.ReactElement[],
+    window?: () => Window,
 };
 
 const Layout = ({ children, navItems, window }: LayoutProps): React.ReactElement => {
-	const trigger = useScrollTrigger({
-		disableHysteresis: true,
-		threshold: 0,
-		target: window !== undefined ? window() : undefined
-	});
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window !== undefined ? window() : undefined
+    });
 
-	const classes = useStyles();
-	const scrollToTopRef = useRef<HTMLDivElement>(null);
+    const classes = useStyles();
+    const scrollToTopRef = useRef<HTMLDivElement>(null);
 
-	const appBar = React.cloneElement((
-		<AppBar>
-			<Toolbar ref={scrollToTopRef}>
-				<Typography variant="h5">Nadun De Silva</Typography>
-				{navItems}
-			</Toolbar>
-		</AppBar>
-	), {
-		elevation: trigger ? 4 : 0
-	});
+    const appBar = React.cloneElement((
+        <AppBar>
+            <Toolbar ref={scrollToTopRef}>
+                <Typography variant="h5">Nadun De Silva</Typography>
+                {navItems}
+            </Toolbar>
+        </AppBar>
+    ), {
+        elevation: trigger ? 4 : 0
+    });
 
-	const handleClick = (event: React.MouseEvent<HTMLDivElement>): void => {
-		scrollToTopRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
-	};
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>): void => {
+        scrollToTopRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
+    };
 
-	return (
-		<React.Fragment>
-			<CssBaseline />
-			{appBar}
-			<Toolbar />
-			<Container>
-				<Box my={2}>
-					{children}
-				</Box>
-			</Container>
-			<Zoom in={trigger}>
-				<div onClick={handleClick} role="presentation" className={classes.fab}>
-					<Fab color="secondary" size="small" aria-label="scroll back to top">
-						<KeyboardArrowUpIcon />
-					</Fab>
-				</div>
-			</Zoom>
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            {appBar}
+            <Toolbar />
+            <Container>
+                <Box my={2}>
+                    {children}
+                </Box>
+            </Container>
+            <Zoom in={trigger}>
+                <div onClick={handleClick} role="presentation" className={classes.fab}>
+                    <Fab color="secondary" size="small" aria-label="scroll back to top">
+                        <KeyboardArrowUpIcon />
+                    </Fab>
+                </div>
+            </Zoom>
+        </React.Fragment>
+    );
 };
 
 export default Layout;
