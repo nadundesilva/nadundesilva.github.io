d9ea9c0653d7fb47ed85a67d0bfeeb29babf9c4e
Nadun De Silva <nadunrds@gmail.com>
2021-08-05 22:13:11 +0530

    Add SEO tags

--- _new_site/nadundesilva-website/src/Portal/index.tsx
+++ _new_site/nadundesilva-website/src/Portal/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Home from "./views/Home";
+import { SEO } from "../components";
 
 const Portal = (): React.ReactElement => {
-    return <Home/>;
+    return (
+        <React.Fragment>
+            <SEO/>
+            <Home/>
+        </React.Fragment>
+    );
 };
 
 export default Portal;
