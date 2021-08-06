7e0ad19f0be5a3dfa2d5fa7d5507f38037d8d693
Nadun De Silva <nadunrds@gmail.com>
2021-08-06 21:33:47 +0530

    Add Certifications section

--- _new_site/nadundesilva-website/src/Portal/index.tsx
+++ _new_site/nadundesilva-website/src/Portal/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Home from "./Home";
 import { SEO } from "../components";
+import Certifications from "./Certifications";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Portal = (): React.ReactElement => {
     return (
         <React.Fragment>
             <SEO/>
-            <Home/>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact>
+                        <Home/>
+                    </Route>
+                    <Route path="/certifications" exact>
+                        <Certifications/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </React.Fragment>
     );
 };
--- _new_site/nadundesilva-website/src/components/Layout/theme.tsx
+++ _new_site/nadundesilva-website/src/components/Layout/theme.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createTheme, Theme } from "@material-ui/core";
-import { blue, indigo } from "@material-ui/core/colors";
+import { indigo } from "@material-ui/core/colors";
 
 type ThemeType = "light" | "dark";
 
@@ -10,7 +10,7 @@ const useTheme = (themeType: ThemeType): Theme => (
             palette: {
                 type: themeType,
                 primary: indigo,
-                secondary: blue
+                secondary: indigo
             }
         }),
         [themeType]
