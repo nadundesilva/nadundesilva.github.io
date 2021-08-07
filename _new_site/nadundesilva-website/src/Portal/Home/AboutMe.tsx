49044b0b4955ba7d0b7b261224c6c2f1564a4d0e
Nadun De Silva <nadunrds@gmail.com>
2021-08-07 13:25:35 +0530

    Fix ABout Me section responsiveness issues

--- _new_site/nadundesilva-website/src/Portal/Home/AboutMe.tsx
+++ _new_site/nadundesilva-website/src/Portal/Home/AboutMe.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Avatar, createStyles, Divider, Grid, makeStyles, Theme, Typography } from "@material-ui/core";
+import { Avatar, createStyles, Divider, Grid, GridSize, Hidden, makeStyles, Theme, Typography } from "@material-ui/core";
 import profilePicture from "../../components/images/profile-photo.jpg";
 
 const useStyles = makeStyles((theme: Theme) => {
     const descriptionSeparatorMargin = 2;
     return createStyles({
-        sectionContent: {
-            padding: theme.spacing(3)
-        },
         descriptionSeparator: {
             marginTop: theme.spacing(descriptionSeparatorMargin),
             marginBottom: theme.spacing(descriptionSeparatorMargin)
@@ -24,40 +21,39 @@ const useStyles = makeStyles((theme: Theme) => {
 
 const AboutMe = (): React.ReactElement => {
     const classes = useStyles();
+    const profilePhoto = (gridWidth: GridSize): React.ReactElement => (
+        <Grid item xs={gridWidth}>
+            <Avatar alt="Nadun De Silva" src={profilePicture} className={classes.profilePhoto}/>
+        </Grid>
+    );
+    const contactItem = (name: string, value: string): React.ReactElement => (
+        <Grid item xs={12} md={4}>
+            <Typography className={classes.contactInfoItemHeader}>
+                {name}:
+            </Typography>
+            <Typography>{value}</Typography>
+        </Grid>
+    );
     return (
-        <Grid container spacing={3} justifyContent="center" alignItems="center" className={classes.sectionContent}>
-            <Grid item xs={8}>
-                I currently work at WSO2 on leading the Observability area in an upcoming
-                Cloud Platform. I have several years of working experience as a software
-                engineer working on multiple Research &amp; Development projects at WSO2.
-                I love learning about new technologies and is particularly interested
-                in Deep Learning.
-
+        <Grid container spacing={3} justifyContent="center" alignItems="center">
+            <Hidden mdUp>{profilePhoto(12)}</Hidden>
+            <Grid item xs={12} md={8}>
+                <Typography align={"justify"}>
+                    I currently work at WSO2 on leading the Observability area
+                    in an upcoming Cloud Platform. I have several years of
+                    working experience as a software engineer working on
+                    multiple Research &amp; Development projects at WSO2. I love
+                    earning about new technologies and is particularly
+                    interested in Deep Learning.
+                </Typography>
                 <Divider className={classes.descriptionSeparator} />
                 <Grid container spacing={3} justifyContent="center">
-                    <Grid item xs={4}>
-                        <Typography className={classes.contactInfoItemHeader}>
-                            Phone:
-                        </Typography>
-                        <Typography>+94 778 222 607</Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Typography className={classes.contactInfoItemHeader}>
-                            Email:
-                        </Typography>
-                        <Typography>nadunrds@gmail.com</Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Typography className={classes.contactInfoItemHeader}>
-                            Website:
-                        </Typography>
-                        <Typography>nadundesilva.github.io</Typography>
-                    </Grid>
+                    {contactItem("Phone", "+94 778 222 607")}
+                    {contactItem("Email", "nadunrds@gmail.com")}
+                    {contactItem("Website", "nadundesilva.github.io")}
                 </Grid>
             </Grid>
-            <Grid item xs={3}>
-                <Avatar alt="Nadun De Silva" src={profilePicture} className={classes.profilePhoto}/>
-            </Grid>
+            <Hidden smDown>{profilePhoto(4)}</Hidden>
         </Grid>
     );
 };
