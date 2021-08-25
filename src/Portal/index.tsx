91cdc8920dec9376ccbc165bbff999505a7bd369
Nadun De Silva <nadunrds@gmail.com>
2021-08-25 18:04:35 +0530

    Move seo optimization tag add logic to index.html

--- src/Portal/index.tsx
+++ src/Portal/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Home from "./Home";
-import { SEO } from "../components";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 const Portal = (): React.ReactElement => {
     return (
         <React.Fragment>
-            <SEO/>
             <BrowserRouter>
                 <Switch>
                     <Route path="/" exact>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,9 @@
 import Heading from "./Heading";
 import Layout, { LayoutContent, useScrollOffset } from "./Layout";
-import SEO from "./SEO";
 
 export {
     Heading,
     Layout,
     LayoutContent,
-    useScrollOffset,
-    SEO
+    useScrollOffset
 };
