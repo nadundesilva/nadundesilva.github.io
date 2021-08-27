7b312eab47e9a612717c50cd2e4866d2a0b0d898
Nadun De Silva <nadunrds@gmail.com>
2021-08-27 22:46:44 +0530

    Remove service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from "react";
-import * as serviceWorkerRegistration from "../src/service-worker/serviceWorkerRegistration";
-import reportWebVitals from "../src/service-worker/reportWebVitals";
+import React from "react";
 import type { AppProps } from "next/app";
 import "../styles.css";
 
 function WebsiteApp({ Component, pageProps }: AppProps): JSX.Element {
-    useEffect(() => {
-        serviceWorkerRegistration.register();
-        reportWebVitals();
-    }, []);
-
     return (
         <React.StrictMode>
             <Component {...pageProps}/>
