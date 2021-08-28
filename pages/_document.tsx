8d5fdcad81452dfc0110d591dea4c964c1e5b507
Nadun De Silva <nadunrds@gmail.com>
2021-08-28 12:39:59 +0530

    Add Roboto font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,7 @@ class WebsiteDocument extends Document {
                     <meta name="description" content={DESCRIPTION}/>
 
                     <link rel="icon" href={`${PUBLIC_URL}/favicon.png`}/>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
                     <link rel="canonical" href={PUBLIC_URL}/>
