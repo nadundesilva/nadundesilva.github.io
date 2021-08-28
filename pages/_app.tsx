78d41876b2812d1c46dee55f4ea6abda0944a01c
Nadun De Silva <nadunrds@gmail.com>
2021-08-28 22:14:25 +0530

    Setup google tag manager for Core Web Vitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import Head from "next/head";
 import "../styles.css";
 
@@ -12,6 +12,15 @@ function WebsiteApp({ Component, pageProps }: AppProps): JSX.Element {
             <Component {...pageProps}/>
         </React.StrictMode>
     );
+}
+
+export function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric): void {
+    (window as any).gtag("event", name, {
+        event_category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+        value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+        event_label: id,
+        non_interaction: true
+    });
 }
 
 export default WebsiteApp;
