f4bc9d7739ea8b3919d6886f91bde5fdc5ca13fd
Nadun De Silva <nadunrds@gmail.com>
2021-08-28 14:22:04 +0530

    Add service worker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        disable: process.env.NODE_ENV === "development",
+    },
     images: {
         loader: "imgix",
         path: "https://nadundesilva.imgix.net/",
     },
     productionBrowserSourceMaps: true
-}
+});
