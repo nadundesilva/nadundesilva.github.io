62b20f2d12eb4e1508ad77dc309485168079991f
Nadun De Silva <nadunrds@gmail.com>
2021-09-12 10:52:13 +0530

    Move all complex styling to styled components and sx prop

--- components/Home/Achievements.tsx
+++ components/Home/Achievements.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import classNames from "classnames";
 import { Box, Container, Grid, Grow, Hidden, ImageList, ImageListItem, Typography } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-import createStyles from "@mui/styles/createStyles";
+import { styled } from "@mui/material/styles";
 import { useScrollOffset } from "@/components/Layout";
 import Image from "next/image";
 
@@ -12,31 +10,21 @@ const hsbcYouthEnterpriseAwards2015 = "/assets/achievements/hsbc-youth-enterpris
 const deansList2017 = "/assets/achievements/deans-list-2017.jpg";
 const angelHack2016 = "/assets/achievements/angel-hack-2016.jpg";
 
-const useStyles = makeStyles(() =>
-    createStyles({
-        imageListItem: {
-            height: "auto"
-        },
-        imageListItemImageContainer: {
-            position: "relative",
-            "&:hover": {
-                "& $imageListItemImageOverlay": {
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    backgroundColor: "rgba(0, 0, 0, 0.6)",
-                    zIndex: 1
-                }
-            }
-        },
-        imageListItemImageOverlay: {
-            color: "#ffffff",
-            position: "absolute",
-            textAlign: "center"
-        }
-    })
-);
+const PREFIX = "Home-Achievements";
+const classes = {
+    imageListItemImageOverlay: `${PREFIX}-imageListItemImageOverlay`
+};
+
+const FullSizeImageListItem = styled(ImageListItem)({
+    width: "100%",
+    height: "auto"
+});
+
+const ImageListItemImageOverlay = styled(Grid)({
+    color: "#ffffff",
+    position: "absolute",
+    textAlign: "center"
+});
 
 const NASA_SPACE_APPS_2017_TITLE = "Global Finalist - Galactic Impact - NASA Space Apps Challenge 2017";
 const PLACEMENTS_IN_DEANS_LIST_TITLE = "Placements in Dean’s List";
@@ -53,7 +41,6 @@ interface AchievementSection {
 const ROW_HEIGHT = 300;
 
 const Achievements = (): React.ReactElement => {
-    const classes = useStyles();
     const { ref: rootRef, offset } = useScrollOffset<HTMLDivElement>();
 
     const achievementSections: AchievementSection[] = [
@@ -87,21 +74,35 @@ const Achievements = (): React.ReactElement => {
     const renderImageListItem = (achievementIndex: number, rowCount: number, totalColumns: number): React.ReactElement => {
         const achievementSection: AchievementSection = achievementSections[achievementIndex];
         return (
-            <ImageListItem rows={rowCount} cols={1}
-                className={classNames(classes.imageListItem, classes.imageListItemImageContainer)}
+            <FullSizeImageListItem rows={rowCount} cols={1}
+                sx={{
+                    position: "relative",
+                    "&:hover": {
+                        [`& .${classes.imageListItemImageOverlay}`]: {
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            backgroundColor: "rgba(0, 0, 0, 0.6)",
+                            zIndex: 1
+                        }
+                    }
+                }}
             >
                 <Grow in={offset > 0.3} timeout={1000 * achievementSection.animationTimeoutFactor}>
                     <Box sx={{ height: "100%" }}>
-                        <Grid container justifyContent="center" alignItems="center" className={classes.imageListItemImageOverlay}>
+                        <ImageListItemImageOverlay container justifyContent="center" alignItems="center"
+                            className={classes.imageListItemImageOverlay}
+                        >
                             <Grid item xs={6}>
                                 <Typography sx={{ fontWeight: "bold" }}>{achievementSection.title}</Typography>
                             </Grid>
-                        </Grid>
+                        </ImageListItemImageOverlay>
                         <Image src={achievementSection.imageLink} alt={achievementSection.title} layout="fill" objectFit="cover"
                             sizes={`${Math.ceil(100 / totalColumns)}vw`}/>
                     </Box>
                 </Grow>
-            </ImageListItem>
+            </FullSizeImageListItem>
         );
     };
 
@@ -111,19 +112,19 @@ const Achievements = (): React.ReactElement => {
         <Container maxWidth={false} disableGutters={true} ref={rootRef}>
             <Hidden mdDown>
                 <ImageList rowHeight={ROW_HEIGHT} cols={LARGE_SCREEN_COLUMN_COUNT}>
-                    <ImageListItem rows={2} cols={1} className={classes.imageListItem}>
+                    <FullSizeImageListItem rows={2} cols={1}>
                         <ImageList rowHeight={ROW_HEIGHT} cols={1}>
                             {renderImageListItem(0, 1, LARGE_SCREEN_COLUMN_COUNT)}
                             {renderImageListItem(1, 1, LARGE_SCREEN_COLUMN_COUNT)}
                         </ImageList>
-                    </ImageListItem>
+                    </FullSizeImageListItem>
                     {renderImageListItem(2, 2, LARGE_SCREEN_COLUMN_COUNT)}
-                    <ImageListItem rows={2} cols={1} className={classes.imageListItem}>
+                    <FullSizeImageListItem rows={2} cols={1}>
                         <ImageList rowHeight={ROW_HEIGHT} cols={1}>
                             {renderImageListItem(3, 1, LARGE_SCREEN_COLUMN_COUNT)}
                             {renderImageListItem(4, 1, LARGE_SCREEN_COLUMN_COUNT)}
                         </ImageList>
-                    </ImageListItem>
+                    </FullSizeImageListItem>
                 </ImageList>
             </Hidden>
             <Hidden mdUp>
--- components/Layout/LayoutContent.tsx
+++ components/Layout/LayoutContent.tsx
@@ -1,36 +1,25 @@
 import React from "react";
-import { Container, Theme } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-import createStyles from "@mui/styles/createStyles";
+import { Container } from "@mui/material";
 import RouterBreadcrumbs from "./RouterBreadcrumbs";
-
-const useStyles = makeStyles((theme: Theme) => {
-    const horizontalPadding = (padding: number): { paddingLeft: string, paddingRight: string } => ({
-        paddingLeft: theme.spacing(padding),
-        paddingRight: theme.spacing(padding)
-    });
-    return createStyles({
-        content: {
-            marginBottom: theme.spacing(5),
-            [theme.breakpoints.down("lg")]: horizontalPadding(0),
-            [theme.breakpoints.up("lg")]: horizontalPadding(30),
-            [theme.breakpoints.up("xl")]: horizontalPadding(60)
-        }
-    });
-});
 
 interface LayoutContentProps {
     children: React.ReactNode,
 }
 
-const LayoutContent = ({ children }: LayoutContentProps): React.ReactElement => {
-    const classes = useStyles();
-    return (
-        <Container maxWidth={false} disableGutters={true} className={classes.content}>
-            <RouterBreadcrumbs/>
-            {children}
-        </Container>
-    );
-};
+const LayoutContent = ({ children }: LayoutContentProps): React.ReactElement => (
+    <Container maxWidth={false} disableGutters={true}
+        sx={{
+            marginBottom: 5,
+            px: {
+                xs: 0,
+                lg: 30,
+                xl: 60
+            }
+        }}
+    >
+        <RouterBreadcrumbs/>
+        {children}
+    </Container>
+);
 
 export default LayoutContent;
