a05a5506b9baa1ed429a619faf42d731c1ff3b3d
Nadun De Silva <nadunrds@gmail.com>
2021-10-05 23:12:12 +0530

    Add match snapshots command and plugins

--- cypress/index.d.ts
+++ cypress/index.d.ts
@@ -4,5 +4,6 @@ declare namespace Cypress {
     interface Chainable {
         getByTestId(value: string): Chainable<Element>;
         childrenByTestId(value: string): Chainable<Element>;
+        matchImageSnapshot(name: string): Chainable<Element>;
     }
 }
