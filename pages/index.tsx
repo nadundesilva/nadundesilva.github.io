00efaf273c0517c9116ad16278c7c1acdd8fc884
Nadun De Silva <nadunrds@gmail.com>
2022-03-12 12:09:51 +0530

    disable failing OSSAR analysis

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-    Box,
-    CircularProgress,
-    Container,
-    styled,
-} from "@mui/material";
+import { Box, CircularProgress, Container, styled } from "@mui/material";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import React, { useRef, useState } from "react";
