62538832b5cbdc18ca2aa7ced74f33888f7236cb
nadundesilva <nadunrds@gmail.com>
2023-08-31 12:07:04 +0000

    [Automated] Update Site (Source Commit: c3d4fd0efdc43dd710eab32acea9c7aface87016)

---
+++ _next/static/_8HGZG58F7y3H4OXZZnys/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ce27af32daf4e13f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
