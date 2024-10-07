7b2180eab683d04d423b9aa94d7d594105089691
nadundesilva <10029121+nadundesilva@users.noreply.github.com>
2024-10-07 09:39:00 +0000

    [Automated] Update Site (Source Commit: 00b1d3b76cde90d683e14f80563954f1e68e4e72)

---
+++ _next/static/_cyF3IJKWa5teZuzLAjvv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-daa80f5b4d0fe27d.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
